5514c70effeedecf7f60b3ee6e1d260dee035b99
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-11-12 15:48:18 -0800

    uploaded investment data

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,5 @@
 class Investment < ApplicationRecord
 end
+
+#FOR CSV UPLOAD FOR NOW - use this in PG admin
+#COPY investments(ticker, name, price, yield, MER, risk_category, objective, distribution, style, created_at, updated_at) FROM '/Users/Birdperson/Desktop/smart-wealth/investments.csv' DELIMITER ',' HEADER CSV
