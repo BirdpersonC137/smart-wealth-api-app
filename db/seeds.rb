3b3488c34d52a55d8659e35598e80731c55be9ff
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-11-14 09:58:41 -0800

    added csv to seeds- works great

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,33 @@
-require 'csv'
+require "csv"
+Investment.destroy_all
+
+CSV.foreach(Rails.root.join('investmentcsv.csv'), headers: true) do |row|
+    p row  
+  Investment.create! do |investment|
+  investment.ticker = row[0]
+  investment.name = row['name']
+  investment.price = row['price']
+  investment.yield = row['yield']
+  investment.mer = row['mer']
+  investment.risk_category = row['risk_category']
+  investment.objective = row['objective']
+  investment.distribution = row['distribution']
+  investment.style = row['style']
+  investment.typeof = row['typeof']
+  puts "#{investment.price}, #{investment.name} saved"
+  end
+end
+
+puts "There are now #{Investment.count} rows in the transactions table"
+
+# params(
+# :ticker, 
+# :name, 
+# :price, 
+# :yield, 
+# :mer, 
+# :risk_category, 
+# :objective, 
+# :distribution, 
+# :style, 
+# :typeof)
