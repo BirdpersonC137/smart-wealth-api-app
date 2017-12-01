791ed8bc5e85d3f6d9c41c1cd3c91499f3264cdd
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-11-30 19:55:20 -0800

    user accounts

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,5 +1,15 @@
 class UserAccountsController < InheritedResources::Base
-
+  def new
+    @user_account = UserAccount.new
+  end
+  def create
+    @user_account = UserAccount.new user_account_params
+    if @user_account.save
+      redirect_to portfolios_path
+    else
+      redirect_to new_user_account_path
+    end
+  end
   private
 
     def user_account_params
