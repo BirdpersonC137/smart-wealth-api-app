af5dbb58f12f5a257f877f34b2c5725a1131b336
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-11-30 19:55:20 -0800

    user accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protected
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || investments_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || new_user_account_path
   end
 end
