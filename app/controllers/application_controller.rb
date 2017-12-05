e28c97173546afa1673778e721520ed1eaa6b73b
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-12-04 19:18:22 -0800

    after sign up path and after sign in path separated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
   protected
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || new_user_account_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || user_dashboard_index_path
   end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :date_of_birth, :password,
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :portfolios
-  devise_for :users
+  devise_for :users, controllers: {:registrations => "registrations"}
   resources :investments
   root 'home#index'
-  patch("/chooseportfolio/:id", to: 'user_accounts#getportfolio')    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
