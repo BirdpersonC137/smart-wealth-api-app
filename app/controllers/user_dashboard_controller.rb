b92d37e55cc3297cd60d9551a130887e60cf5177
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-12-06 18:09:15 -0800

    dashboard polishing

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -2,6 +2,6 @@ class UserDashboardController < ApplicationController
     def index
         @user = current_user
         @user_accounts = UserAccount.where(user_id: @user.id)
-        #@user_account = UserAccount.find_by [params: :id]
+        @user_account = UserAccount.where(user_id: @user.id).first       
     end
 end
