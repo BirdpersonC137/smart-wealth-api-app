9273e5b315e905cf39c811fd63fa11f0a89f1959
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2017-12-07 11:06:50 -0800

    minor refactoring

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -2,6 +2,11 @@ class UserDashboardController < ApplicationController
     def index
         @user = current_user
         @user_accounts = UserAccount.where(user_id: @user.id)
-        @user_account = UserAccount.where(user_id: @user.id).first       
+        @user_account = UserAccount.find_by_user_id (@user.id)
+    end
+    def deposits
+        @user = current_user
+        @user_accounts = UserAccount.where(user_id: @user.id)
+        @user_account = UserAccount.find_by_user_id (@user.id)              
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :portfolios
   devise_for :users, controllers: {:registrations => "registrations"}
   resources :investments
+  get '/deposits', to: 'user_dashboard#deposits'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
